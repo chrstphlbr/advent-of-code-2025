a6e4482beb5503eb5f5e6dd46f09ac0215132357
Christoph Laaber <christoph.laaber@gmail.com>
2025-12-02 11:12:21 +0100

    added CLI structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Advent of Code 2025");
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "aoc25", author, version, about = "Advent of Code 2025")]
+struct Cli {
+    day: u8,
+}
+
+fn main() -> Result<()> {
+    println!("Welcome to Advent of Code 2025!\n");
+
+    let cli = Cli::parse();
+
+    println!("Run puzzle from day {}", cli.day);
+
+    Ok(())
 }
